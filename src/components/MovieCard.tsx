ad0d98fbc3ddd0650635631fc208eab68e482546
JiEun <orbital0m0@gmail.com>
2025-08-29 13:25:38 +0900

    fix: moviecard 버튼으로 변경

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,33 +1,40 @@
 import { Link } from "react-router-dom";
 import { MovieCardProps } from "../types/Carousel";
 import useMovieCard from "../hooks/useMovieCard";
+import Button from "./Button";
 
 const MovieCard = (props: MovieCardProps) => {
   const { handleGenreClick, generateRandomGradient } = useMovieCard(props);
 
   if (props.type === "genres") {
     return (
-      <div
+      <Button
         className={`slider-card slider-card-${props.layout}`}
         onClick={handleGenreClick}
-        role="button"
-        tabIndex={0}
-        aria-label={`${props.slide.name} 장르 선택`}
-        style={{ cursor: "pointer" }}
-      >
-        <div
-          className="slider-image-container"
-          style={{
-            background: generateRandomGradient,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        ></div>
-        <div className={`slider-content slider-content-${props.layout}`}>
-          <h3 className="slider-title">{props.slide.name}</h3>
-        </div>
-      </div>
+        value={
+          <>
+            <div
+              className="slider-image-container"
+              style={{
+                background: generateRandomGradient,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                outline: 0,
+                border: 0,
+              }}
+            ></div>
+            <div className={`slider-content slider-content-${props.layout}`}>
+              <h3 className="slider-title">{props.slide.name}</h3>
+            </div>
+          </>
+        }
+      />
     );
   } else {
     return (
