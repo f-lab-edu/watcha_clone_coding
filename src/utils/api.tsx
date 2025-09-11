e15a7bec7ec93dcc68bb4a41251fd9772960076c
orbital0m0 <orbital0m0@gmail.com>
2025-09-11 15:55:00 +0900

    refactor: 코드 단순화화

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -2,49 +2,50 @@ import { MovieData } from "@/types/Movie";
 import { instance } from "./axios";
 import { transformMovieList, transformMovieData } from "@/utils/transform";
 
-export const fetchPopularMovieList = async () => {
-  const response = await instance.get("/movie/popular?language=ko-KR&page=1");
-  return transformMovieList(response.data.results);
-};
+export const fetchPopularMovieList = () => 
+  instance
+    .get("/movie/popular?language=ko-KR&page=1")
+    .then((response) => transformMovieList(response.data.results));
 
-export const fetchTopRatedMovieList = async () => {
-  const response = await instance.get("/movie/top_rated?language=ko-KR&page=1");
-  return transformMovieList(response.data.results);
-};
+export const fetchTopRatedMovieList = () => 
+  instance
+    .get("/movie/top_rated?language=ko-KR&page=1")
+    .then((response) => transformMovieList(response.data.results));
 
-export const fetchNowPlayingMovieList = async () => {
-  const response = await instance.get("/movie/now_playing?language=ko-KR&page=1");
-  return transformMovieList(response.data.results);
-};
+export const fetchNowPlayingMovieList = () => 
+  instance
+    .get("/movie/now_playing?language=ko-KR&page=1")
+    .then((response) => transformMovieList(response.data.results));
 
-export const fetchMovieDetail = async (movieId: string) => {
-  const response = await instance.get(
-    `/movie/${movieId}?append_to_response=credits,videos,belongs_to_collection&language=ko-KR`,
-  );
-  return transformMovieData(response.data);
-};
+export const fetchMovieDetail = (movieId: string) => 
+  instance
+    .get(`/movie/${movieId}?append_to_response=credits,videos,belongs_to_collection&language=ko-KR`)
+    .then((response) => transformMovieData(response.data));
 
-export const fetchMovieReviews = async (movieId: string) => {
-  const response = await instance.get(`/movie/${movieId}/reviews?language=en-US&page=1`);
-  return response.data;
-};
+export const fetchMovieReviews = (movieId: string) => 
+  instance
+    .get(`/movie/${movieId}/reviews?language=en-US&page=1`)
+    .then((response) => response.data);
 
-export const fetchTodayTrendingMovie = async () => {
-  const response = await instance.get("/trending/movie/day?language=ko-KR");
-  return response.data.results;
-};
+export const fetchTodayTrendingMovie = () => 
+  instance
+    .get("/trending/movie/day?language=ko-KR")
+    .then((response) => response.data.results);
 
-export const fetchMovieGenres = async () => {
-  const response = await instance.get("/genre/movie/list?language=ko");
-  return response.data.genres;
-};
+export const fetchMovieGenres = () => 
+  instance
+    .get("/genre/movie/list?language=ko")
+    .then((response) => response.data.genres);
 
-export const fetchSearchKeywords = async (query: string) => {
-  const response = await instance.get(`/search/movie?query=${query}&include_adult=false&language=ko-KR&page=1`);
-  return transformMovieList(response.data.results, { usePoster: true });
-};
 
-export const fetchSearchGenres = async (genreId: string) => {
-  const response = await instance.get(`/discover/movie?with_genres=${genreId}&page=1&language=ko-KR`);
-  return transformMovieList(response.data.results, { usePoster: true });
-};
+export const fetchSearchKeywords = (query: string) => 
+  instance
+    .get(`/search/movie?query=${query}&include_adult=false&language=ko-KR&page=1`)
+    .then((response) => transformMovieList(response.data.results, { usePoster: true }));
+
+
+export const fetchSearchGenres = (genreId: string) => 
+  instance
+    .get(`/discover/movie?with_genres=${genreId}&page=1&language=ko-KR`)
+    .then((response) => transformMovieList(response.data.results, { usePoster: true }));
+
