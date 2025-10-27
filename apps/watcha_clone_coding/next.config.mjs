2aac221850603e0a83b2d1ae9a052fd8e81e0a58
orbital0m0 <orbital0m0@gmail.com>
2025-10-27 15:49:42 +0900

    next.config.mjs 파일에서 outDir: "standalone" 옵션을 제거하여 기본 출력 설정을 사용하도록 변경

--- apps/watcha_clone_coding/next.config.mjs
+++ apps/watcha_clone_coding/next.config.mjs
@@ -1,25 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // Single-Page Application (SPA) 출력. -> SSR 적용을 위해 비활성화
-    output: 'standalone', // 독립 실행형 출력 모든 의존성 번들링
-    distDir: './dist', // 빌드 출력 디렉터리를 `./dist/`로 변경합니다.
-    transpilePackages: ['@watcha/carousel'], // 트랜스파일링할 패키지 목록
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'image.tmdb.org',
-          pathname: '/t/p/**',
-        },
-      ],
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    }
+  distDir: './dist', // 빌드 출력 디렉터리를 `./dist/`로 변경합니다.
+  transpilePackages: ['@watcha/carousel'], // 트랜스파일링할 패키지 목록
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   }
-   
-  export default nextConfig
+}
+ 
+export default nextConfig
